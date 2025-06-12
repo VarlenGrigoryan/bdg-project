e9388a94f049baa8654a827e993ade204e49b7a3
VarlenGrigoryan <varlen.grigoryan05@mail.ru>
2025-06-13 00:55:33 +0400

    Update outputs.tf

--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -2,3 +2,8 @@ output "instance_public_ip" {
   description = "Public IP of the EC2 instance"
   value       = aws_instance.web.public_ip
 }
+
+output "ec2_public_ip" {
+  value = aws_instance.web.public_ip
+  description = "The public IP of the EC2 instance"
+}
