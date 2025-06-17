23021b602c09b57bfa9d029047b28bb38b644251
varlen <varlen.grigoryan05@mail.ru>
2025-06-17 17:09:36 +0400

    Fix Ansible role and workflow for Docker Compose deployment

--- terraform/main.tf
+++ terraform/main.tf
@@ -33,9 +33,35 @@ resource "aws_instance" "web" {
   instance_type = "t2.micro"
   key_name      = aws_key_pair.deployer.key_name
   subnet_id     = data.aws_subnet.default.id
+  iam_instance_profile = aws_iam_instance_profile.ec2_instance_profile.name
   tags = {
     Name = "varlens-web-instance"
   }
+}
+
+resource "aws_iam_role" "ec2_ecr_role" {
+  name = "ec2-ecr-access-role"
+
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [{
+      Effect = "Allow"
+      Principal = {
+        Service = "ec2.amazonaws.com"
+      }
+      Action = "sts:AssumeRole"
+    }]
+  })
+}
+
+resource "aws_iam_role_policy_attachment" "ec2_ecr_policy" {
+  role       = aws_iam_role.ec2_ecr_role.name
+  policy_arn = "arn:aws:iam::aws:policy/AmazonEC2ContainerRegistryReadOnly"
+}
+
+resource "aws_iam_instance_profile" "ec2_instance_profile" {
+  name = "ec2-instance-profile"
+  role = aws_iam_role.ec2_ecr_role.name
 }
 
 terraform {
